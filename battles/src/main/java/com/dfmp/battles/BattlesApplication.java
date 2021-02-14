a4f23baee203cc3951c20c13184564dc72d56998
chbarbosa <carloshenriquefb@gmail.com>
2021-02-14 13:22:20 -0300

    battle service added

---
+++ battles/src/main/java/com/dfmp/battles/BattlesApplication.java
@@ -0,0 +1,13 @@
+package com.dfmp.battles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattlesApplication.class, args);
+	}
+
+}
---
+++ battles/src/test/java/com/dfmp/battles/BattlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfmp.battles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
